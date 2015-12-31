740ba04cfe7eed076493f2b92612fc31f0580aef
ice1000 <951394653@qq.com>
2015-12-31 20:41:12 +0800

    I was using chinese....How silly

--- app/src/main/java/util/OnItemClickListener.java
+++ app/src/main/java/util/OnItemClickListener.java
@@ -7,6 +7,7 @@ import android.view.View;
  * Copyright 2015(c) Comet Corporation.
  * Created by asus1 on 2015/12/31.
  */
+
 public interface OnItemClickListener {
     void onItemClick(View view, int position);
     void onItemLongClick(View view , int position);
