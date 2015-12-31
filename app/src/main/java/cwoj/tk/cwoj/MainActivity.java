dd111629b822f02f2d5a8259be68294dbb8274ec
千里冰封 <zhangyinsen@126.com>
2015-12-31 15:55:53 +0800

    增加设置类

--- app/src/main/java/cwoj/tk/cwoj/MainActivity.java
+++ app/src/main/java/cwoj/tk/cwoj/MainActivity.java
@@ -1,18 +1,16 @@
 package cwoj.tk.cwoj;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Toolbar toolbar;
+//    private Toolbar toolbar;
     private WebView webView;
 
     @Override
@@ -20,19 +18,19 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        toolbar = (Toolbar) findViewById(R.id.toolbar);
         webView = (WebView) findViewById(R.id.webView);
-        setSupportActionBar(toolbar);
+//        setSupportActionBar(toolbar);
 
         webView.loadUrl("http://www.cwoj.tk");
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 
     @Override
@@ -55,5 +53,9 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void settings(View view){
+        startActivity(new Intent(MainActivity.this, SettingsActivity.class));
     }
 }
